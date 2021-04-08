467d316418902dc10190331a87892a73ea221095
Cole Sturza <colesturza@gmail.com>
2021-04-08 00:11:05 -0600

    add 2d and 1d fast poisson solver

---
+++ Matlab/poisson_1d.m
@@ -0,0 +1,68 @@
+clear
+close all
+
+% -u_xx = -sin(x)
+% u = -sin(x)
+
+u = @(x) -sin(x);  % actual solution
+f = @(x) -sin(x); % rhs of ODE
+
+a = 0; b = 2*pi;
+alpha = 0; beta = -sin(2*pi);
+
+N = 100;
+x = linspace(a, b, N);
+h = x(2) - x(1);
+
+% K = diag(2*ones(N-2,1))-diag(ones(N-3,1),-1)-diag(ones(N-3,1),1);
+
+rhs = h^2 * f(x(2:end-1));
+rhs(1) = rhs(1) + alpha;
+rhs(end) = rhs(end) + beta;
+
+% sol = K\b';  % slow method
+
+% [S, Lam] = eig(K);  % remove later
+% sol = S * inv(Lam) * inv(S) * b';  % inefficient way fast solver
+
+n = length(rhs);
+L = 2*(1-cos((1:n)*pi/(n+1))); 
+LL = diag(1./L);
+
+% i = (1:n)';
+% ii = i * i';
+% S = sin(ii*pi/(n+1)) * sqrt(2/(n+1));  % extra term makes eigenvectors orthonormal
+
+% sol = S * LL * inv(S) * b';  % inefficient way fast solver
+
+% norm(fast_sine_transform(b') - inv(S) * b')
+
+% v = fast_sine_transform(b');
+% y = inv(S) * b';
+% semilogy(x(2:end-1), abs(v), x(2:end-1), abs(y));
+
+b1 = fast_sine_transform(rhs');
+b2 = LL * b1;
+sol = fast_sine_transform(b2);
+
+uapp =[alpha; sol; beta];
+
+uex = u(x);
+
+figure(1)
+plot(x,uapp,x,uex)
+figure(2)
+plot(x,abs(uapp-uex'),'o-')
+err = abs(uapp-uex');
+
+% fast sine transform for a 1D vector v
+function y = fast_sine_transform(v)
+    n = length(v);
+    % extra term makes eigenvectors orthonormal so we only need one
+    % transform for both ifft and fft (Q)
+    const = -sqrt(2/(n+1));
+    % need to shift the vales of the vector
+    v1 = [0; v; zeros(n+1, 1)];
+    v2 = const * imag(fft(v1));
+    y = v2(2:n+1);
+end
---
+++ Matlab/poisson_2d.m
@@ -0,0 +1,59 @@
+clear
+close all
+
+% -u_xx - u_yy = 2sin(x)sin(y)
+% u = sin(x)sin(y)
+
+u = @(x, y) sin(x).*sin(y);  % actual solution
+f = @(x, y) 2 * sin(x).*sin(y); % rhs of ODE
+
+a = 0;
+b = 2*pi;
+c = 0;
+d = 2*pi;
+
+alpha = 0;
+beta = 0;
+gamma = 0;
+delta = 0;
+
+N = 100;
+[x, y] = meshgrid(linspace(a, b, N), linspace(a, b, N));
+h = x(1,1) - x(1, 2);  % h is the same in both the y and x direction
+
+% K = diag(2*ones(N-2,1))-diag(ones(N-3,1),-1)-diag(ones(N-3,1),1);
+% K2D = kron(eye(N), K) + kron(K, eye(N));
+
+rhs = h^2 * f(x(2:end-1, 2:end-1), y(2:end-1, 2:end-1));
+
+[k, l] = size(rhs);
+L = 2*(1-cos((1:k)*pi/(k+1))) + 2*(1-cos((1:l)*pi/(l+1))); 
+LL = diag(1./L);
+
+rhs1 = fast_sine_transform(rhs);
+rhs1 = fast_sine_transform(rhs1');
+rhs2 = LL * rhs1;
+rhs2 = fast_sine_transform(rhs2);
+sol = fast_sine_transform(rhs2');
+
+uapp = [alpha * ones(1, l); sol; beta * ones(1, l)];
+uapp = [gamma * ones(k+2, 1) uapp delta * ones(k+2, 1)];
+
+uex = u(x, y);
+
+figure(1)
+mesh(x, y, uapp)
+figure(2)
+mesh(x, y, abs(uapp-uex))
+
+% fast sine transform for a matrix V
+function Y = fast_sine_transform(V)
+    [m, n] = size(V);
+    % extra term makes eigenvectors orthonormal so we only need one
+    % transform for both ifft and fft (Q)
+    const = -sqrt(2/(n+1));
+    % need to shift the vales of the matrix
+    V1 = [zeros(1,m); V; zeros(n+1,m)];
+    V2 = const * imag(fft(V1));
+    Y = V2(2:n+1, :);
+end
